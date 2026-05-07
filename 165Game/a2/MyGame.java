adced8ccba696529bc283b652e7191c21fc6e504
Church <bchurch2@csus.edu>
2026-05-07 14:37:07 -0700

    photo logic removed, houses added, terrain expanded

--- 165Game/a2/MyGame.java
+++ 165Game/a2/MyGame.java
@@ -10,8 +10,6 @@ import tage.Camera;
 import tage.CameraOrbit3D;
 import tage.input.InputManager;
 import tage.input.action.AbstractInputAction;
-import tage.nodeControllers.RotationController;
-import tage.nodeControllers.ShakeSinkController;
 import tage.Viewport;
 import tage.shapes.*;
 import tage.networking.IGameConnection.ProtocolType;
@@ -53,22 +51,10 @@ public class MyGame extends VariableFrameRateGame
 	private ProtocolType serverProtocol = ProtocolType.UDP;
 
 	//light stuff
-	private Light light1, p1Light, p2Light, p3Light, homeLight;
+	private Light light1, homeLight;
 
-	//pyramid stuff
-	private ObjShape pyramidS;
-	private GameObject pyramid1, pyramid2, pyramid3;
-	private TextureImage pyramid1tx, pyramid2tx, pyramid3tx;
-
-	//home stuff
-	private GameObject home;
-	private ObjShape homeS;
-	private TextureImage homeTx;
-
-	private GameObject homeMarkerTop;
-	private ObjShape homeMarkerS;
-	private TextureImage homeMarkerTx;
-	private RotationController homeMarkerRotation;
+	//home stuff - logical position only, no rendered object
+	private Vector3f homePosition = new Vector3f(0f, 0f, -10f);
 
 	//ground stuff
 	private ObjShape groundS;
@@ -76,12 +62,17 @@ public class MyGame extends VariableFrameRateGame
 	private TextureImage groundTx;
 	private TextureImage groundHeightMap;
 
+	//house stuff
+	private ObjShape houseS;
+	private GameObject[] houses = new GameObject[5];
+	private TextureImage houseTx;
+
 	//broom object
 	private ObjShape broomS;	
 	private GameObject broom;
 
 	//control stuff
-	private float moveSpeed = 5.0f;
+	private float moveSpeed = 15.0f;
 
 	//camera stuff
 	private CameraOrbit3D orbitController;
@@ -97,30 +88,17 @@ public class MyGame extends VariableFrameRateGame
 
 	private boolean overheadInitialized = false;
 	private boolean overheadManualPan = false;
-
-	//axis stuff
-	private GameObject xAxis, yAxis, zAxis;
-	private boolean axesVisible = true;
-
-	//photo and crash stuff
-	private boolean gameOver = false;
-	private boolean gameWon = false;
-	private String statusMsg = "";
-	
-	private ObjShape photoS;
-	
-	private float crashRangeP = 4.0f;
-	private float photoRange = 7.0f;
-
-	private int score = 0;
-
-	private ArrayList<GameObject> photosOnDolphin = new ArrayList<>();
-	private boolean[] pyramidPic = new boolean[3];
-	private boolean picsPlaced = false;
-
-	private GameObject[] wallPics = new GameObject[3];
+	private float[][] housePositions = {
+		{  60f,  70f},
+		{ -80f,  40f},
+		{ 100f, -60f},
+		{ -40f, -100f},
+		{  20f,  120f}
+	};
 
 	//game stuff
+	private boolean gameOver = false;
+	private String statusMsg = "";
 	private boolean gameStart = false;
 
 	//skybox stuff
@@ -165,12 +143,9 @@ public class MyGame extends VariableFrameRateGame
 	public void loadShapes()
 	{	dolS = new ImportedModel("dolphinHighPoly.obj");
 		witchS = new ImportedModel("witchModel.obj");
-		pyramidS = new ManualPyramid();
-		photoS = new Plane();
-		homeS = new Home();
 		groundS = new TerrainPlane(1000);
+		houseS = new ImportedModel("house.obj");
 		broomS = new ImportedModel("broomModel.obj");
-		homeMarkerS = new ManualPyramid();
 		ghostS = new ImportedModel("dolphinHighPoly.obj");  //TODO: change when we have multiple player models
 	}
 
@@ -178,18 +153,12 @@ public class MyGame extends VariableFrameRateGame
 	public void loadTextures()
 	{	doltx = new TextureImage("Dolphin_HighPolyUV.jpg");
 	
-		//load pyramid stuff
-		pyramid1tx = new TextureImage( "taterTots.jpg");
-		pyramid2tx = new TextureImage("makemake.jpg");
-		pyramid3tx = new TextureImage("eris.jpg");
-
-		//load home stuff
-		homeTx = new TextureImage("brick1.jpg");
-		homeMarkerTx = new TextureImage("ice.jpg");
-
 		//load ground stuff
 		groundTx = new TextureImage("sand.jpg");
 		groundHeightMap = new TextureImage("hills.jpg");
+
+		//load house stuff
+		houseTx = new TextureImage("brick1.jpg");
 
 		//load ghost stuff
 		ghostTx = new TextureImage("Dolphin_HighPolyUV.jpg");  //TODO: change when we have multiple player modelsS
@@ -216,75 +185,24 @@ public class MyGame extends VariableFrameRateGame
 		dol.setLocalTranslation(initialTranslation);
 		applyAvatarTransform();
 
-		//build ground
+		//build ground - single large terrain tile
 		ground = new GameObject(GameObject.root(), groundS, groundTx);
-		initialTranslation = (new Matrix4f()).translation(0f, -8f, 0f);
-		initialScale = (new Matrix4f()).scaling(40.0f, 6.0f, 40.0f);
-		ground.setLocalTranslation(initialTranslation);
-		ground.setLocalScale(initialScale);
+		ground.setLocalTranslation(new Matrix4f().translation(0f, -16f, 0f));
+		ground.setLocalScale(new Matrix4f().scaling(160.0f, 6.0f, 160.0f));
 		ground.setIsTerrain(true);
 		ground.setHeightMap(groundHeightMap);
 
-		//build pyramids
-		pyramid1 = new GameObject(GameObject.root(), pyramidS, pyramid1tx);
-		initialTranslation = (new Matrix4f()).translation(-15f, -8.5f, -20f);
-		initialScale = (new Matrix4f()).scaling(2.0f);
-		pyramid1.setLocalTranslation(initialTranslation);
-		pyramid1.setLocalScale(initialScale);
-
-		pyramid2 = new GameObject(GameObject.root(), pyramidS, pyramid2tx);
-		initialTranslation = (new Matrix4f()).translation(20f, -8.5f, 0f);
-		initialScale = (new Matrix4f()).scaling(2.0f);
-		pyramid2.setLocalTranslation(initialTranslation);
-		pyramid2.setLocalScale(initialScale);
-
-		pyramid3 = new GameObject(GameObject.root(), pyramidS, pyramid3tx);
-		initialTranslation = (new Matrix4f()).translation(25f, -8.5f, 25f);
-		initialScale = (new Matrix4f()).scaling(2.0f);
-		pyramid3.setLocalTranslation(initialTranslation);
-		pyramid3.setLocalScale(initialScale);
-
-		//world axes
-		float axisLength = 100.0f;
-
-		//red x-axis
-		ObjShape xLine = new Line(new Vector3f(0f, 0f, 0f), new Vector3f(axisLength, 0f, 0f));
-		xAxis = new GameObject(GameObject.root(), xLine);
-		xAxis.getRenderStates().setColor(new Vector3f(1.0f, 0.0f, 0.0f));
-
-		//green y-axis
-		ObjShape yLine = new Line(new Vector3f(0f, 0f, 0f), new Vector3f(0f, axisLength, 0f));
-		yAxis = new GameObject(GameObject.root(), yLine);
-		yAxis.getRenderStates().setColor(new Vector3f(0.0f, 1.0f, 0.0f));
-
-		//blue z-axis
-		ObjShape zLine = new Line(new Vector3f(0f, 0f, 0f), new Vector3f(0f, 0f, axisLength));
-		zAxis = new GameObject(GameObject.root(), zLine);
-		zAxis.getRenderStates().setColor(new Vector3f(0.0f, 0.0f, 1.0f));
-
-		//home build
-		home = new GameObject(GameObject.root(), homeS, homeTx);
-		home.setLocalTranslation(new Matrix4f().translation(0f, 3.1f, -10f)); 
-		home.setLocalScale(new Matrix4f().scaling(0.5f));
-		home.getRenderStates().setColor(new Vector3f(1f, 1f, 1f));
+		//build 5 houses scattered across the map
+		for (int i = 0; i < 5; i++) {
+			houses[i] = new GameObject(GameObject.root(), houseS, houseTx);
+			houses[i].setLocalTranslation(new Matrix4f().translation(housePositions[i][0], -16, housePositions[i][1]));
+			houses[i].setLocalScale(new Matrix4f().scaling(0.02f));
+		}
 
 		//broom object in the world
 		broom = new GameObject(GameObject.root(), broomS, null);
 		broom.setLocalTranslation(new Matrix4f().translation(-8f, 1.0f, 10f));
 		broom.setLocalScale(new Matrix4f().scaling(0.5f));
-
-		//home marker build
-		homeMarkerTop = new GameObject(home, homeMarkerS, homeMarkerTx);
-		homeMarkerTop.setLocalTranslation(new Matrix4f().translation(0f, 5.0f, 0f));
-		homeMarkerTop.setLocalScale(new Matrix4f().scaling(1.5f));
-		homeMarkerTop.pitch((float)Math.toRadians(180.0f));
-		homeMarkerTop.getRenderStates().setColor(new Vector3f(1f, 1f, 1f));
-
-		//rotation controller for home marker
-		homeMarkerRotation = new RotationController(engine, new Vector3f(0.0f, 1.0f, 0.0f), 0.001f);
-		homeMarkerRotation.addTarget(homeMarkerTop);
-		homeMarkerRotation.enable();
-		engine.getSceneGraph().addNodeController(homeMarkerRotation);
 
 	}
 
@@ -324,22 +242,8 @@ public class MyGame extends VariableFrameRateGame
 		light1.setLocation(new Vector3f(5.0f, 4.0f, 2.0f));
 		(engine.getSceneGraph()).addLight(light1);
 
-		//pyramid lights
-		p1Light = new Light();
-		Vector3f p1Pos = pyramid1.getWorldLocation();
-		p1Light.setLocation(new Vector3f(p1Pos).add(0f, 10f, 0f));
-
-		p2Light = new Light();
-		Vector3f p2Pos = pyramid2.getWorldLocation();
-		p2Light.setLocation(new Vector3f(p2Pos).add(0f, 10f, 0f));
-
-		p3Light = new Light();
-		Vector3f p3Pos = pyramid3.getWorldLocation();
-		p3Light.setLocation(new Vector3f(p3Pos).add(0f, 10f, 0f));
-
 		homeLight = new Light();
-		Vector3f hp = home.getWorldLocation();
-		homeLight.setLocation(new Vector3f(hp).add(0f, 12f, 0f));
+		homeLight.setLocation(new Vector3f(homePosition).add(0f, 12f, 0f));
 	}
 
 	@Override
@@ -365,7 +269,7 @@ public class MyGame extends VariableFrameRateGame
 		elapsTime = (currFrameTime - lastFrameTime) / 1000.0f;
 
 		//game over check
-		if (gameOver && !gameWon) {
+		if (gameOver) {
 			updateMainHUD();
 			updateOverheadHUD();
 			return;
@@ -381,10 +285,6 @@ public class MyGame extends VariableFrameRateGame
 		float terrainHeight = ground.getHeight(dolLoc.x, dolLoc.z);
 		dol.setLocalLocation(new Vector3f(dolLoc.x, terrainHeight + avatarGroundOffset, dolLoc.z));
 
-		if (gameWon && !picsPlaced) {
-			statusMsg = "Press SPACE to hang your photos!";
-		}
-		
 		Vector3f dolPos = dol.getWorldLocation();
 		orbitController.updateCameraPosition();
 
@@ -407,33 +307,7 @@ public class MyGame extends VariableFrameRateGame
 
 		updateOverheadCamera();
 
-		//collision stuff
-		Vector3f p1Pos = pyramid1.getWorldLocation();
-		Vector3f p2Pos = pyramid2.getWorldLocation();
-		Vector3f p3Pos = pyramid3.getWorldLocation();
-
-		float d1 = dolPos.distance(p1Pos);
-		float d2 = dolPos.distance(p2Pos);
-		float d3 = dolPos.distance(p3Pos);
-
-		float minDist = Math.min(d1, Math.min(d2, d3));
-
-		if (!gameWon && !picsPlaced) {
-			if (minDist < crashRangeP) {
-				statusMsg = "You've crashed! GAME OVER!";
-				gameOver = true;
-			} else if (minDist < photoRange) {
-				statusMsg = "Press P to take picture";
-			} else {
-				statusMsg = "Not close enough, move towards a pyramid";
-			}
-		} else if (gameWon && !picsPlaced) {
-			if (isHome()) statusMsg = "Press SPACE to hang up your photos!";
-			else statusMsg = "Return home and press SPACE to put your pictures on the wall!";
-		} else if (picsPlaced) {
-			statusMsg = "The Pictures are all hung up! You Won!!";
-		}
-
+		statusMsg = "Explore the world!";
 
 		//update huds
 		updateMainHUD();
@@ -444,25 +318,9 @@ public class MyGame extends VariableFrameRateGame
 	private void updateMainHUD()
 	{
 		Viewport mainVp = engine.getRenderSystem().getViewport("LEFT");
-
 		int hudX = (int) mainVp.getActualLeft() + 15;
 		int hudY = (int) (mainVp.getActualBottom() - mainVp.getActualHeight());
-
-		Vector3f statusColor = new Vector3f(1,1,0); // default is yellow
-
-		if (gameOver && !gameWon)
-			statusColor = new Vector3f(1,0,0); // red if lose
-		else if (gameOver && gameWon)
-			statusColor = new Vector3f(0,1,0); // green if win
-
-		String text = "Score: " + score + "      " + statusMsg;
-
-		(engine.getHUDmanager()).setHUD1(
-			text,
-			statusColor,
-			hudX,
-			hudY
-		);
+		(engine.getHUDmanager()).setHUD1(statusMsg, new Vector3f(1,1,0), hudX, hudY);
 	}
 
 	//update overhead hud
@@ -501,7 +359,7 @@ public class MyGame extends VariableFrameRateGame
 		if (dol == null) return;
 		if (selectedAvatar == 1) {
 			dol.setShape(witchS);
-			dol.setTextureImage(homeTx);
+			dol.setTextureImage(null);
 			statusMsg = "Witch selected";
 		}
 		else {
@@ -525,122 +383,6 @@ public class MyGame extends VariableFrameRateGame
 			dol.setLocalScale(new Matrix4f().scaling(3.0f));
 			dol.setLocalRotation(new Matrix4f());
 		}
-	}
-
-	private class ToggleAxesAction extends AbstractInputAction {
-		@Override
-		public void performAction(float time, Event e) {
-			axesVisible = !axesVisible;
-
-			if (axesVisible) {
-				xAxis.getRenderStates().enableRendering();
-				yAxis.getRenderStates().enableRendering();
-				zAxis.getRenderStates().enableRendering();
-			}
-			else {
-				xAxis.getRenderStates().disableRendering();
-				yAxis.getRenderStates().disableRendering();
-				zAxis.getRenderStates().disableRendering();
-			}
-		}
-	}
-
-	private void tryTakePicture() {
-
-		GameObject[] pyramids = { pyramid1, pyramid2, pyramid3 };
-		TextureImage[] texs = { pyramid1tx, pyramid2tx, pyramid3tx};
-		Vector3f dolPos = dol.getWorldLocation();
-
-		int bestIndex = -1;
-		float bestDist = Float.MAX_VALUE;
-
-		for (int i = 0; i < 3; i++) {
-			if (pyramidPic[i]) continue;
-			
-			float d = dolPos.distance(pyramids[i].getWorldLocation());
-			
-			if (d < bestDist) {
-				bestDist = d;
-				bestIndex = i;
-			}
-		}
-
-		if (bestIndex == -1) {
-			statusMsg = "All pictures already taken!";
-			return;
-		}
-
-		if (bestDist > photoRange) {
-			statusMsg = "Not close enough to take a picture.";
-			return;
-		}
-
-		GameObject photo = new GameObject(dol, photoS, texs[bestIndex]);
-		photo.applyParentRotationToPosition(true);
-
-		int idx = photosOnDolphin.size();
-
-		float stripForward = -1.0f;
-		float stripUp = 0.8f; 
-		float spacing = 0.35f;                
-
-		float x = (idx - 1) * spacing;            
-		float y = stripUp;
-		float z = stripForward;
-
-		photo.setLocalLocation(new Vector3f(x, y, z));
-		photo.setLocalScale(new Matrix4f().scaling(0.05f, 0.05f, 0.05f));
-
-
-		photosOnDolphin.add(photo);
-		score++;
-
-		statusMsg = "Picture taken! Score: " + score;
-
-		if (score >= 3) {
-			statusMsg = "All pictures taken! Return home and press SPACE to put pictures on the wall!";
-			gameWon = true;
-		}
-	}
-
-
-	private void placePhotosOnWalls() {
-		if (picsPlaced) {
-			statusMsg = "Photos already placed!";
-			return;
-		}
-
-		picsPlaced = true;
-
-		Vector3f[] spots = new Vector3f[] {
-			new Vector3f(-1.2f, 0.5f, -3.0f),  
-			new Vector3f( 0.0f, 0.5f, -3.0f),  
-			new Vector3f( 1.2f, 0.5f, -3.0f)   
-		};
-
-		TextureImage[] texs = new TextureImage[] { pyramid1tx, pyramid2tx, pyramid3tx };
-
-		for (int i = 0; i < 3; i++) {
-			wallPics[i] = new GameObject(home, photoS, texs[i]);
-			wallPics[i].setLocalLocation(spots[i]);
-			wallPics[i].setLocalScale(new Matrix4f().scaling(1.0f, 1.0f, 1.0f));
-
-			wallPics[i].pitch((float)Math.toRadians(90.0f));
-		}
-
-		for (GameObject pic : photosOnDolphin) {
-			pic.getRenderStates().disableRendering();
-		}
-
-		statusMsg = "You placed the photos on the wall! You win!";
-		gameWon = true;
-		gameOver = true; 
-	}
-
-	private boolean isHome() {
-		Vector3f dolPos = dol.getWorldLocation();
-		Vector3f homePos = home.getWorldLocation();
-		return dolPos.distance(homePos) < 5.0f;
 	}
 
 	private class OrbitAzimuthAction extends AbstractInputAction {
@@ -854,31 +596,6 @@ public class MyGame extends VariableFrameRateGame
 		}
 	}
 
-	//TAKE OUT THE HOP OFF ELEMENT, ONLY MAKE IT SO SPACE PUTS THE PICTURES UP ON WALL
-	private class ToggleRideAction extends AbstractInputAction {
-		@Override
-		public void performAction(float time, Event e) {
-
-			if (gameWon && isHome()) {
-				placePhotosOnWalls();
-				return;
-        	}
-
-			if (gameWon && !isHome()) {
-				statusMsg = "Return home before trying to place your pictures!";
-			}
-
-			statusMsg = "Take all pyramid photos first, then return home!";
-		}
-	}
-
-	private class TakePictureAction extends AbstractInputAction {
-		@Override
-		public void performAction(float time, Event e) {
-			tryTakePicture();
-		}
-	}
-
 	private void setupNetworking()
 	{
 		isConnected = false;
@@ -921,12 +638,6 @@ public class MyGame extends VariableFrameRateGame
 		OrbitElevationAction downAction = new OrbitElevationAction(-1.0f);
 		OrbitZoomAction zoomInAction = new OrbitZoomAction(-1.0f);
 		OrbitZoomAction zoomOutAction = new OrbitZoomAction(1.0f);
-
-		// ----- Keyboard toggle for axis visibility -----
-		im.associateActionWithAllKeyboards(
-			net.java.games.input.Component.Identifier.Key.X,
-			new ToggleAxesAction(),
-			InputManager.INPUT_ACTION_TYPE.ON_PRESS_ONLY);
 
 		// ----- Keyboard Game Control -----
 		im.associateActionWithAllKeyboards(
@@ -1032,16 +743,6 @@ public class MyGame extends VariableFrameRateGame
 			new KeyboardAction("yaw", -1f),
 			InputManager.INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
 
-		im.associateActionWithAllKeyboards(
-			net.java.games.input.Component.Identifier.Key.SPACE,
-			new ToggleRideAction(),
-			InputManager.INPUT_ACTION_TYPE.ON_PRESS_ONLY);
-
-		im.associateActionWithAllKeyboards(
-			net.java.games.input.Component.Identifier.Key.P,
-			new TakePictureAction(),
-			InputManager.INPUT_ACTION_TYPE.ON_PRESS_ONLY);
-
 		// ----- Gamepad Dolphin Movement -----
 		im.associateActionWithAllGamepads(
 			net.java.games.input.Component.Identifier.Axis.RY,
@@ -1063,7 +764,7 @@ public class MyGame extends VariableFrameRateGame
 	public ObjShape getDolphinShape() { return dolS; }
 	public TextureImage getDolphinTexture() { return doltx; }
 	public ObjShape getWitchShape() { return witchS; }
-	public TextureImage getWitchTexture() { return homeTx; }
+	public TextureImage getWitchTexture() { return null; }
 	public GhostManager getGhostManager() { return gm; }
 	public Engine getEngine() { return engine; }
 	public Vector3f getPlayerPosition() { return dol.getWorldLocation(); }
